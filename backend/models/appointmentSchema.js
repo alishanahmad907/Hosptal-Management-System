6d5fb1a8baf9d8faaff3d5deccaa89363c1177e3
Ali Shan Ahmad <alishanahmad907@gmail.com>
2024-04-20 15:48:41 +0530

    appointment creation, updation and deletion done

--- backend/models/appointmentSchema.js
+++ backend/models/appointmentSchema.js
@@ -1,9 +1,7 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import validator from "validator";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 
-const userSchema = new mongoose.Schema({
+const appointmentSchema = new mongoose.Schema({
 	firstName: {
 		type: String,
 		requireed: true,
@@ -40,41 +38,45 @@ const userSchema = new mongoose.Schema({
 		required: true,
 		enum: ["Male", "Female"],
 	},
-	password: {
+	appointment_date: {
 		type: String,
 		required: true,
-		minLength: [5, "Password should be greater than 5 characters!"],
-		select: false,
 	},
-	role: {
+	department: {
 		type: String,
 		required: true,
-		enum: ["Admin", "Patient", "Doctor"],
 	},
-	doctorDepartment: {
+	doctor: {
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+	},
+	hasVisited: {
+		type: Boolean,
+		default: false,
+	},
+	doctorId: {
+		type: mongoose.Schema.ObjectId,
+		required: true,
+	},
+	patientId: {
+		type: mongoose.Schema.ObjectId,
+		required: true,
+	},
+	address: {
 		type: String,
+		required: true,
 	},
-	docAvatar: {
-		public_id: String,
-		url: String,
+	status: {
+		type: String,
+		enum: ["Pending", "Accepted", "Rejected"],
+		default: "Pending",
 	},
 });
 
-userSchema.pre("save", async function (next) {
-	if (!this.isModified("password")) {
-		next();
-	}
-	this.password = await bcrypt.hash(this.password, 10);
-});
-
-userSchema.methods.comparePassword = async function (enteredPwd) {
-	return await bcrypt.compare(enteredPwd, this.password);
-};
-
-userSchema.methods.generateJsonWebToken = function () {
-	return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
-		expiresIn: process.env.JWT_EXPIRES,
-	});
-};
-
-export const User = mongoose.model("User", userSchema);
+export const Appointment = mongoose.model("Appointment",appointmentSchema);
