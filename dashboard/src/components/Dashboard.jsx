533e285205163de0863729f120fa1fcf1e52cd57
Ali Shan Ahmad <alishanahmad907@gmail.com>
2024-04-28 11:57:28 +0530

    dashboard started- created add new admin, message and login page

---
+++ dashboard/src/components/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Dashboard = () => {
+
+
+    // if(!isAuthenticated){
+    //     return <Navigate to={"/login"}/>;
+    // }
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
